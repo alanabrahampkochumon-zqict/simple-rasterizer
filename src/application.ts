8504c0a7a3e0f5ea858d5cdab82cd74b8b564d91
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-06-19 16:03:04 +0530

    feat: add line drawing through generic interpolation

--- src/application.ts
+++ src/application.ts
@@ -70,17 +70,25 @@ export class Application {
      */
     #putPixel(x: number, y: number, color: IVec3) {
         const colorChannels = 4
+        // Without rounding the floating point math can throw off indices
+        // often times creating jittery lines, or nothing besides a dot
         y = Math.round(y)
         x = Math.round(x)
         const flatIndex = colorChannels * (y * this.width + x)
-        if (flatIndex > this.targetSurface.data.length) {
-            // console.log(`Out of bound access. Index: ${flatIndex}, Size: ${this.targetSurface.data.length}`)
-            return
+
+        // Only color if the flatIndex is in-bounds
+        // Since we need to move at least 3 places from the flatIndex,
+        // we are ensuring that there is enough indices eg: if size is 500 and flatIndex is 496
+        // then we can use 496, 497, 498, 499(last index), but if its 497, then i + 3 is out-of-bounds
+        flatIndex < this.targetSurface.data.length - 3 && blitColor(this.targetSurface.data);
+
+        function blitColor(buffer: ImageDataArray) {
+            buffer[flatIndex] = color.r;
+            buffer[flatIndex + 1] = color.g;
+            buffer[flatIndex + 2] = color.b;
+            buffer[flatIndex + 3] = 255; // Full opacity on alpha channel
         }
-        this.targetSurface.data[flatIndex] = color.r;
-        this.targetSurface.data[flatIndex + 1] = color.g;
-        this.targetSurface.data[flatIndex + 2] = color.b;
-        this.targetSurface.data[flatIndex + 3] = 255; // Full opacity on alpha channel
+
     }
 
     /**
@@ -99,8 +107,8 @@ export class Application {
     }
 
     render() {
-        this.colorUV()
-        this.drawLineBox()
+        this.colorUVTest()
+        this.drawLineTest()
     }
 
     #drawLineH(p0: Vec2, p1: Vec2, color: IVec3) {
@@ -140,7 +148,7 @@ export class Application {
         }
 
         // Slope is flipped, so m = δx/δy
-        const m = ( p1.x - p0.x ) / (p1.y - p0.y)
+        const m = (p1.x - p0.x) / (p1.y - p0.y)
         let x = p0.x
 
         for (let y = p0.y; y < p1.y; ++y) {
@@ -149,13 +157,87 @@ export class Application {
         }
     }
 
-    drawLine(p0: Vec2, p1: Vec2, color: IVec3) {
+    /**
+     * Draws a line between p0 and p1.
+     *
+     * @privateRemarks Left for benchmarking
+     *
+     * @param p0 The first point to draw the line.
+     * @param p1 The second point to draw the line.
+     * @param color The color to paint the line stroke.
+     * @private
+     */
+    #drawLineSlow(p0: Vec2, p1: Vec2, color: IVec3) {
         const deltaX = Math.abs(p1.x - p0.x)
         const deltaY = Math.abs(p1.y - p0.y)
         if (deltaX > deltaY) {
             this.#drawLineH(p0, p1, color)
         } else {
             this.#drawLineV(p0, p1, color)
+        }
+    }
+
+
+    /**
+     * Interpolates between two values returning the interpolated values as a list.
+     * @param a0 The initial value for x.
+     * @param b0 The initial value for y.
+     * @param a1 The final value for x.
+     * @param b1 The final value for y.
+     *
+     * @return The interpolated values for the y (second term).
+     */
+    interpolate(a0: number, b0: number, a1: number, b1: number) {
+        if (a0 == a1) {
+            return [b0]
+        }
+        const interpolatedValues = []
+
+        const m = (b1 - b0) / (a1 - a0)
+        let y = b0
+
+        for(let x = a0; x <= a1; ++x) {
+            interpolatedValues.push(y)
+            y += m
+        }
+
+        return interpolatedValues
+    }
+
+    drawLine(p0: Vec2, p1: Vec2, color: IVec3) {
+        const deltaX = Math.abs(p1.x - p0.x)
+        const deltaY = Math.abs(p1.y - p0.y)
+        if (deltaX > deltaY) {
+            // Line is horizontalish (i.e, there are more x values
+            // so we can use x to iterating interpolating y
+
+            // If initial x value is greater then, we need to swap as we are looping from
+            // smaller value to bigger value
+            if (p0.x > p1.x) {
+                // TODO: Extract
+                const temp = p0
+                p0 = p1
+                p1 = temp
+            }
+
+            const values = this.interpolate(p0.x, p0.y, p1.x, p1.y)
+            for(let x = p0.x; x <= p1.x; ++x) {
+                this.#putPixel(x, values[x - p0.x], color)
+            }
+        } else {
+            // Line is verticalish
+            // i.e, there are more vertical points to interpolate
+            if (p0.y > p1.y) {
+                // TODO: Extract
+                const temp = p0
+                p0 = p1
+                p1 = temp
+            }
+
+            const values = this.interpolate(p0.y, p0.x, p1.y, p1.x)
+            for(let y = p0.y; y <= p1.y; ++y)
+                this.#putPixel(values[y - p0.y], y, color)
+
         }
     }
 
@@ -174,7 +256,7 @@ export class Application {
      *********************
      */
 
-    drawLineBox() {
+    drawLineTest() {
         const topLeft = new Vec2(10, 10)
         const bottomLeft = new Vec2(10, 500)
         const topRight = new Vec2(500, 10)
@@ -206,7 +288,18 @@ export class Application {
         // // Right to left line
         // this.drawLineV(new Vec2(800, 100), new Vec2(50, 50), new IVec3(255, 0, 255))
 
-
+        // this.#drawLineSlow(new Vec2(topLeft.x, topLeft.y), new Vec2(topRight.x, topRight.y), new IVec3(255, 0, 255)) // Horizontal Line
+        // this.#drawLineSlow(new Vec2(topLeft.x, topLeft.y), new Vec2(bottomLeft.x, bottomLeft.y), new IVec3(255, 0, 255)) // Can't draw vertical line since slope == 0
+        // this.#drawLineSlow(new Vec2(topLeft.x, topLeft.y), new Vec2(bottomRight.x, bottomRight.y), new IVec3(255, 0, 255))// Cross
+        //
+        // this.#drawLineSlow(new Vec2(bottomLeft.x, bottomLeft.y), new Vec2(bottomRight.x, bottomRight.y), new IVec3(255, 0, 255)) // Bottom Horizontal line
+        // this.#drawLineSlow(new Vec2(topRight.x, topRight.y), new Vec2(bottomRight.x, bottomRight.y), new IVec3(255, 0, 255)) // Can't draw vertical line since slope == 0
+        // this.#drawLineSlow(new Vec2(bottomLeft.x, bottomLeft.y), new Vec2(topRight.x, topRight.y), new IVec3(255, 0, 255)) // Cross
+        //
+        // this.#drawLineSlow(new Vec2(0, 0), new Vec2(this.width, this.height), new IVec3(255, 255, 255))
+        //
+        // // Right to left line
+        // this.#drawLineSlow(new Vec2(800, 100), new Vec2(50, 50), new IVec3(255, 255, 0))
 
         this.drawLine(new Vec2(topLeft.x, topLeft.y), new Vec2(topRight.x, topRight.y), new IVec3(255, 0, 255)) // Horizontal Line
         this.drawLine(new Vec2(topLeft.x, topLeft.y), new Vec2(bottomLeft.x, bottomLeft.y), new IVec3(255, 0, 255)) // Can't draw vertical line since slope == 0
@@ -216,14 +309,15 @@ export class Application {
         this.drawLine(new Vec2(topRight.x, topRight.y), new Vec2(bottomRight.x, bottomRight.y), new IVec3(255, 0, 255)) // Can't draw vertical line since slope == 0
         this.drawLine(new Vec2(bottomLeft.x, bottomLeft.y), new Vec2(topRight.x, topRight.y), new IVec3(255, 0, 255)) // Cross
 
-        // Right to left line
         this.drawLine(new Vec2(0, 0), new Vec2(this.width, this.height), new IVec3(255, 255, 255))
 
+        // Right to left line
         this.drawLine(new Vec2(800, 100), new Vec2(50, 50), new IVec3(255, 255, 0))
+        this.drawLine(new Vec2(500, 500), new Vec2(450, 40), new IVec3(255, 255, 0))
 
     }
 
-    colorUV() {
+    colorUVTest() {
         // Test Code: Renders UV
         for (let i = 0; i < this.height; ++i)
             for (let j = 0; j < this.width; ++j)
