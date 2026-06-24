b49ba5d3ebe2e34f293868c6c1131571acb4b18c
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-06-24 18:25:30 +0530

    feat: add basic Mat4 Arithmetic ops

--- src/math/mat4.ts
+++ src/math/mat4.ts
@@ -33,7 +33,101 @@ export class Mat4 {
     }
 
 
+    static Add(res: Mat4, a: Mat4, b: Mat4): Mat4 {
+        res.data[0] = a.data[0] + b.data[0]
+        res.data[1] = a.data[1] + b.data[1]
+        res.data[2] = a.data[2] + b.data[2]
+        res.data[3] = a.data[3] + b.data[3]
 
+        res.data[4] = a.data[4] + b.data[4]
+        res.data[5] = a.data[5] + b.data[5]
+        res.data[6] = a.data[6] + b.data[6]
+        res.data[7] = a.data[7] + b.data[7]
 
+        res.data[8] = a.data[8] + b.data[8]
+        res.data[9] = a.data[9] + b.data[9]
+        res.data[10] = a.data[10] + b.data[10]
+        res.data[11] = a.data[11] + b.data[11]
+
+        res.data[12] = a.data[12] + b.data[12]
+        res.data[13] = a.data[13] + b.data[13]
+        res.data[14] = a.data[14] + b.data[14]
+        res.data[15] = a.data[15] + b.data[15]
+
+        return res;
+    }
+
+    static Sub(res: Mat4, a: Mat4, b: Mat4): Mat4 {
+        res.data[0] = a.data[0] - b.data[0]
+        res.data[1] = a.data[1] - b.data[1]
+        res.data[2] = a.data[2] - b.data[2]
+        res.data[3] = a.data[3] - b.data[3]
+
+        res.data[4] = a.data[4] - b.data[4]
+        res.data[5] = a.data[5] - b.data[5]
+        res.data[6] = a.data[6] - b.data[6]
+        res.data[7] = a.data[7] - b.data[7]
+
+        res.data[8] = a.data[8] - b.data[8]
+        res.data[9] = a.data[9] - b.data[9]
+        res.data[10] = a.data[10] - b.data[10]
+        res.data[11] = a.data[11] - b.data[11]
+
+        res.data[12] = a.data[12] - b.data[12]
+        res.data[13] = a.data[13] - b.data[13]
+        res.data[14] = a.data[14] - b.data[14]
+        res.data[15] = a.data[15] - b.data[15]
+
+        return res;
+    }
+
+    static Mul(res: Mat4, a: Mat4, s: number): Mat4 {
+        res.data[0] = a.data[0] * s
+        res.data[1] = a.data[1] * s
+        res.data[2] = a.data[2] * s
+        res.data[3] = a.data[3] * s
+
+        res.data[4] = a.data[4] * s
+        res.data[5] = a.data[5] * s
+        res.data[6] = a.data[6] * s
+        res.data[7] = a.data[7] * s
+
+        res.data[8] = a.data[8] * s
+        res.data[9] = a.data[9] * s
+        res.data[10] = a.data[10] * s
+        res.data[11] = a.data[11] * s
+
+        res.data[12] = a.data[12] * s
+        res.data[13] = a.data[13] * s
+        res.data[14] = a.data[14] * s
+        res.data[15] = a.data[15] * s
+
+        return res;
+    }
+
+    static Div(res: Mat4, a: Mat4, s: number): Mat4 {
+        const factor = 1.0 / s
+        res.data[0] = a.data[0] * factor
+        res.data[1] = a.data[1] * factor
+        res.data[2] = a.data[2] * factor
+        res.data[3] = a.data[3] * factor
+
+        res.data[4] = a.data[4] * factor
+        res.data[5] = a.data[5] * factor
+        res.data[6] = a.data[6] * factor
+        res.data[7] = a.data[7] * factor
+
+        res.data[8] = a.data[8] * factor
+        res.data[9] = a.data[9] * factor
+        res.data[10] = a.data[10] * factor
+        res.data[11] = a.data[11] * factor
+
+        res.data[12] = a.data[12] * factor
+        res.data[13] = a.data[13] * factor
+        res.data[14] = a.data[14] * factor
+        res.data[15] = a.data[15] * factor
+
+        return res;
+    }
 
 }
